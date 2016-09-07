a40f8d6b64bb7323e58a1f517b8bbab4911f39e1
Peter Bilstein <peter.bilstein@syngenio.de>
2016-09-07 11:38:41 +0200

    corrected dependencies

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -145,8 +145,7 @@ module.exports = function (grunt) {
     // Make sure code styles are up to par
     jscs: {
       options: {
-        config: '.jscsrc',
-        verbose: true
+        config: '.jscsrc'
       },
       all: {
         src: [
@@ -178,7 +177,7 @@ module.exports = function (grunt) {
     postcss: {
       options: {
         processors: [
-          require('autoprefixer-core')({browsers: ['last 1 version']})
+          require('autoprefixer')({browsers: ['last 1 version']})
         ]
       },
       server: {
